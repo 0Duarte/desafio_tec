f8277b0dbf020c724bb2b1aaaf6ee3eb7173d4a9
Guilherme <guiduartesbs@hotmail.com>
2025-09-10 20:16:57 -0300

    feat: create initial factories and seeders

--- banking/database/seeders/DatabaseSeeder.php
+++ banking/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
+use App\Models\Customer;
 use App\Models\User;
+use App\Models\Wallet;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +16,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Customer::factory()->count(5)->create()->each(function ($customer) {
+            Wallet::create([
+                'user_id' => $customer->id,
+                'user_type' => Customer::class,
+                'balance' => rand(100, 1000),
+            ]);
+        });
+
+        Company::factory()->count(5)->create()->each(function ($company) {
+            Wallet::create([
+                'user_id' => $company->id,
+                'user_type' => Company::class,
+                'balance' => 0,
+            ]);
+        });
     }
 }
