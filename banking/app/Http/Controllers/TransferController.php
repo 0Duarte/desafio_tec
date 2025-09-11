88d642db26669e6c72e32e07a58ee8cf404d5d02
Guilherme <guiduartesbs@hotmail.com>
2025-09-11 00:31:03 -0300

    feat: create transferController

---
+++ banking/app/Http/Controllers/TransferController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TransferRequest;
+use App\Services\TransferService;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class TransferController extends Controller
+{
+    protected TransferService $service;
+
+    public function __construct(TransferService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function store(TransferRequest $request)
+    {
+        $data = $request->validated();
+
+        try {
+            $transaction = $this->service->transfer($data['payer'], $data['payee'], $data['value']);
+            return response()->json($transaction, 201);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ], 422);
+        }
+    }
+}
