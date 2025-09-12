68950202986b1da303416146e311c1c2cd4d1f2c
Guilherme <guiduartesbs@hotmail.com>
2025-09-11 21:20:34 -0300

    chore: create transfer config with external endpoints

---
+++ banking/config/transfer.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'external_service' => [
+        'authorize' => 'https://util.devi.tools/api/v2/authorize',
+        'notification' => 'https://util.devi.tools/api/v2/notify',
+    ],
+];
